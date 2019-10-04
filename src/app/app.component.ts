c9a6bd71bfd8d22d635e14e529bbf4b47edea7d8
Eugene Stativka <stativkaea@gmail.com>
2019-10-04 12:35:02 +0200

    Init app

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'calc-root',
+  template: `
+    <main>Calculator</main>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'calculator'
+}
